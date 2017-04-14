ae341a4a1a0317dd5228a6d9bfea4ea208682b75
Celso Mireles <celso.mireles@gmail.com>
2017-04-13 22:30:01 -0700

    Sets up docker

--- app/shared/utils/index.js
+++ app/shared/utils/index.js
@@ -1,31 +1,10 @@
 import bcrypt from 'bcrypt';
 
-export const userLevel = 1;
-export const companyAdminLevel = 2;
-export const superAdminLevel = 3;
-
 export const parseBearerToken = (headers) => new Promise((resolve, reject) => {
   if (headers.authorization) {
     resolve(headers.authorization.split(' ')[1]);
   } else {
     reject('No sessionToken found');
-  }
-});
-
-export const checkUserIsInCompany = (userId, employees) => new Promise((resolve, reject) => {
-  const index = employees.indexOf(userId);
-  if (index !== -1) {
-    resolve(userId);
-  } else {
-    reject('Unauthorized');
-  }
-});
-
-export const ensureObjectExists = (obj, msg) => new Promise((resolve, reject) => {
-  if (obj) {
-    resolve(obj);
-  } else {
-    reject(msg || 'Expected object to exist');
   }
 });
 
@@ -40,22 +19,6 @@ export const checkPasswordExists = (user) => new Promise((resolve, reject) => {
 export const comparePassword = (providedPassword, storedPassword) => {
   return bcrypt.compare(providedPassword, storedPassword);
 };
-
-export const ensureIsAdmin = (user) => new Promise((resolve, reject) => {
-  if (user.accessLevel > 1) {
-    resolve(user);
-  } else {
-    reject('No admin access.');
-  }
-});
-
-export const confirmIsSuperAdmin = (accessLevel) => new Promise((resolve, reject) => {
-  if (accessLevel === superAdminLevel) {
-    resolve(true);
-  } else {
-    reject('Not enough access privileges.');
-  }
-});
 
 export const generateRandomNum = () => {
   return Math.floor(Math.random() * 90000) + 10000;
